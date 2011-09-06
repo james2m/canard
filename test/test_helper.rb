84d6ed960b8e255e60f44e4993a8905619cc3ad4
James McCarthy <james2mccarthy@gmail.com>
2011-09-06 13:59:52 +0100

    Setup test helper to run active record.
    Signed-off-by: James McCarthy <james2mccarthy@gmail.com>

---
+++ test/test_helper.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+gem     'minitest'
+require 'active_record'
+require 'minitest/autorun'
+
+module MiniTestWithHooks
+  class Unit < MiniTest::Unit
+    def before_suites
+    end
+
+    def after_suites
+    end
+
+    def _run_suites(suites, type)
+      begin
+        before_suites
+        super(suites, type)
+      ensure
+        after_suites
+      end
+    end
+  end
+end
+
+module MiniTestWithActiveRecord
+  class Unit < MiniTestWithHooks::Unit
+
+    def before_suites
+      super
+      ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+      ActiveRecord::Migration.verbose = false
+      
+      @migration  = Class.new(ActiveRecord::Migration) do
+
+        def change
+          create_table :users, :force => true do |t|
+            t.timestamps
+            t.string     :roles_mask
+          end
+        end
+
+      end
+      
+      @migration.new.migrate(:up)
+    end
+
+    def after_suites
+      @migration.new.migrate(:down)
+      super
+    end
+  end
+end
+
+MiniTest::Unit.runner = MiniTestWithActiveRecord::Unit.new
