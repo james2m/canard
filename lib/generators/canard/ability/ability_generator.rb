568b26347b1f62aea2d8cb4a14f3a9407fbaae70
James McCarthy <james2mccarthy@gmail.com>
2011-09-09 01:28:36 +0100

    Corrected a typo in the abilities spec template.

--- lib/generators/canard/ability/ability_generator.rb
+++ lib/generators/canard/ability/ability_generator.rb
@@ -7,7 +7,7 @@ module Canard
       
       def generate_ability
         template "abilities.rb.erb", "app/abilities/#{file_name}.rb"
-        template "abilities_spec.rb.erb", "spec/abliities/#{file_name}_spec.rb"
+        template "abilities_spec.rb.erb", "spec/abilities/#{file_name}_spec.rb"
       end
       
       private
