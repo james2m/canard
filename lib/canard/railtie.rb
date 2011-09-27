ada8a2ccec03bc4dbe076f4e0c2a693e12b49a79
James McCarthy <james2mccarthy@gmail.com>
2011-09-27 03:31:57 +0100

    Updated the railtie to reload abilities in development.

--- lib/canard/railtie.rb
+++ lib/canard/railtie.rb
@@ -8,12 +8,13 @@ module Canard
       ActiveSupport.on_load :active_record do
         require 'canard/user_model'
         extend Canard::UserModel
+        Canard.abilities_path ||= File.expand_path('app/abilities', Rails.root)
+        Canard.find_abilities
       end
     end
     
-    initializer "canard.find_abilities" do |app|
-      Canard.abilities_path ||= File.expand_path('app/abilities', Rails.root)
-      Canard.find_abilities
+    initializer "canard.abilities_reloading", :after => "action_dispatch.configure" do |app|
+      ActionDispatch::Reloader.to_prepare { Canard.find_abilities }
     end
     
   end
