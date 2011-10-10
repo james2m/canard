2571064fedc4ac7eda8b807e336c373470e97002
James McCarthy <james2mccarthy@gmail.com>
2011-10-10 23:58:27 +0100

    Ensures generated abillity spec names are pluralized.

--- lib/generators/rspec/ability/ability_generator.rb
+++ lib/generators/rspec/ability/ability_generator.rb
@@ -8,14 +8,10 @@ module Rspec
       argument :ability_definitions, :type => :array, :default => [], :banner => "can:abilities:models cannot:abilities:models"
 
       def generate_ability_spec
-        template "abilities_spec.rb.erb", "spec/abilities/#{file_name}_spec.rb"
+        template "abilities_spec.rb.erb", "spec/abilities/#{file_name.pluralize}_spec.rb"
       end
       
       private
-      
-      def add_new_abilities
-        gsub_file "spec/abilities/#{file_name}_spec.rb", /^(\s*end\s*\Z)/, 'wibble' + '\1'
-      end
       
       def definitions(&block)
         ability_definitions.each { |definition| AbilityDefinition.parse(definition) }
