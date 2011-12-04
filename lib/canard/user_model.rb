52dd833eea80e0f7e61d5436b7f757fc35ab0b7c
James McCarthy <james2mccarthy@gmail.com>
2011-12-04 22:22:42 +0000

    Added scopes for each role.

--- lib/canard/user_model.rb
+++ lib/canard/user_model.rb
@@ -8,7 +8,35 @@ module Canard
       options = args.extract_options!.symbolize_keys
 
       roles options[:roles] if options.has_key?(:roles) && column_names.include?(roles_attribute_name.to_s)
+              
+        valid_roles.each do |role|
+          define_scopes_for_role role
+        end
+    
     end
+    
+    private
+    
+    def define_scopes_for_role(role)
+      
+      attribute_name  = %{"#{table_name}"."#{roles_attribute_name}"}
+      include_scope   = role.to_s.pluralize
+      exclude_scope   = "non_#{include_scope}"
+
+      (class << self; self end).class_eval do
+        
+        define_method(include_scope) do
+          where("#{attribute_name} & :role_mask > 0", { :role_mask => mask_for(role) })
+        end
+
+        define_method(exclude_scope) do
+          where("#{attribute_name} & :role_mask = 0 or #{attribute_name} is null", { :role_mask => mask_for(role) })
+        end
+        
+      end
+
+    end
+      
 
   end
 
--- test/user_model_test.rb
+++ test/user_model_test.rb
@@ -55,4 +55,135 @@ describe Canard::UserModel do
     
   end
 
+  describe "scopes" do
+    
+    before do
+      @no_role             = User.create
+      @admin_author_viewer = User.create(:roles => [:admin, :author, :viewer])
+      @author_viewer       = User.create(:roles => [:author, :viewer])
+      @viewer              = User.create(:roles => [:viewer])
+    end
+    
+    after do
+      User.delete_all
+    end
+    
+    describe "on models with roles" do
+      
+      subject { User }
+      
+      it "should add a scope to return instances with each role" do
+        subject.must_respond_to :admins
+        subject.must_respond_to :authors
+        subject.must_respond_to :viewers
+      end
+      
+      it "should add a scope to return instances without each role" do
+        subject.must_respond_to :non_admins
+        subject.must_respond_to :non_authors
+        subject.must_respond_to :non_viewers
+      end
+      
+      describe "finding instances with a role" do
+        
+        describe "admins scope" do
+
+          subject { User.admins }
+
+          it "should return only admins" do
+            subject.must_equal [@admin_author_viewer]
+          end
+
+          it "should not return non admins" do
+            subject.wont_include @no_role
+            subject.wont_include @author_viewer
+            subject.wont_include @viewer
+          end
+
+        end
+
+        describe "authors scope" do
+
+          subject { User.authors }
+
+          it "should return only authors" do
+            subject.must_equal [@admin_author_viewer, @author_viewer]
+          end
+
+          it "should not return non authors" do
+            subject.wont_include @no_role
+            subject.wont_include @viewer
+          end
+
+        end
+
+        describe "viewers scope" do
+
+          subject { User.viewers }
+
+          it "should return only viewers" do
+            subject.must_equal [@admin_author_viewer, @author_viewer, @viewer]
+          end
+
+          it "should not return non authors" do
+            subject.wont_include @no_role
+          end
+
+        end
+
+      end
+      
+      describe "finding instances without a role" do
+        
+        describe "non_admins scope" do
+
+          subject { User.non_admins }
+
+          it "should return only non_admins" do
+            subject.must_equal [@no_role, @author_viewer, @viewer]
+          end
+
+          it "should not return admins" do
+            subject.wont_include @admin_author_viewer
+          end
+
+        end
+
+        describe "non_authors scope" do
+
+          subject { User.non_authors }
+
+          it "should return only non_authors" do
+            subject.must_equal [@no_role, @viewer]
+          end
+
+          it "should not return authors" do
+            subject.wont_include @admin_author_viewer
+            subject.wont_include @author_viewer
+          end
+
+        end
+
+        describe "non_viewers scope" do
+
+          subject { User.non_viewers }
+
+          it "should return only non_viewers" do
+            subject.must_equal [@no_role]
+          end
+
+          it "should not return viewers" do
+            subject.wont_include @admin_author_viewer
+            subject.wont_include @author_viewer
+            subject.wont_include @viewer
+          end
+
+        end
+
+      end
+      
+    end
+
+  end
+  
 end
