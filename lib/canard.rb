89eac2fa89c53741d8febb094547e5b132c8b4a2
James McCarthy <james2mccarthy@gmail.com>
2012-03-10 17:32:03 +0000

    Removed use of autoload in preparation for Ruby 3.
    Signed-off-by: James McCarthy <james2mccarthy@gmail.com>

--- lib/canard.rb
+++ lib/canard.rb
@@ -1,12 +1,9 @@
 require 'cancan'
 require 'role_model'
-require "canard/version"
+require 'canard/version'
+require 'canard/user_model'
 require "canard/find_abilities"
 require "ability"
-
-module Canard
-  autoload :UserModel, 'canard/user_model'
-end
 
 require 'canard/railtie' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
 
