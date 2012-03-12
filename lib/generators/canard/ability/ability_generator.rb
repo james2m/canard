c0eb8d4b141b59f9e612a356d1f83f74c77ce1df
James McCarthy <james2mccarthy@gmail.com>
2012-03-12 12:46:46 +0000

    Updated the generator to create abilities in the Abilities#default_path directory.

--- lib/generators/canard/ability/ability_generator.rb
+++ lib/generators/canard/ability/ability_generator.rb
@@ -7,7 +7,7 @@ module Canard
       argument :ability_definitions, :type => :array, :default => [], :banner => "can:[read,update]:[user,account] cannot:[create,destroy]:user"
 
       def generate_ability
-        template "abilities.rb.erb", Canard.abilities_path + "/#{file_name.pluralize}.rb"
+        template "abilities.rb.erb", Abilities.default_path + "/#{file_name.pluralize}.rb"
       end
     
       hook_for :test_framework, :as => 'ability'
