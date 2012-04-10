28355eb0eb03ae427146f2ecd5c9f471bf5e9780
James McCarthy <james2mccarthy@gmail.com>
2012-04-10 09:38:26 +0100

    Fixed test_helper to work on 1.8.7 and bumped version to 0.2.7.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,9 @@
-require 'rubygems'
-gem 'minitest'
 require 'minitest/autorun'
 
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
 
 Rails.backtrace_cleaner.remove_silencers!
 
