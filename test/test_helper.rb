983a647cb41b0cbe46eaf1dbfaae35909e218995
James McCarthy <james2mccarthy@gmail.com>
2012-05-05 20:49:47 +0100

    Changed test_helper to always drop and reload the test db.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,30 @@
 gem 'minitest'
 require 'minitest/autorun'
+require 'active_record'
 
 # Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
+environment  = ENV["RAILS_ENV"] = 'test'
+rails_root   = File.expand_path('../dummy', __FILE__)
+database_yml = File.expand_path('config/database.yml', rails_root)
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+ActiveRecord::Base.configurations = YAML.load_file(database_yml)
+
+config = ActiveRecord::Base.configurations[environment]
+db     = File.expand_path(config['database'], rails_root)
+
+# Drop the test database and migrate up
+FileUtils.rm(db) if File.exist?(db)
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => db)
+ActiveRecord::Base.connection
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Migrator.up File.expand_path('db/migrate', rails_root)
+
+# Load dummy rails app
+require File.expand_path('config/environment.rb', rails_root)
 
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files (reloadable reloads canard)
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-ActiveRecord::Migration.verbose = false
-load(File.expand_path('../dummy/db/schema.rb', __FILE__))
 
 
